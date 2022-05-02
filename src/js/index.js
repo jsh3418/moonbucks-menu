1d4c41f305beaee4893449f1f6c476aa089e2416
seonghun <albert5428@gmail.com>
2022-05-02 11:31:24 +0900

    step-2.2

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,5 @@
-const $ = (selector) => document.querySelector(selector);
-
-const store = {
-  setLocalStorage(menu) {
-    localStorage.setItem("menu", JSON.stringify(menu));
-  },
-  getLocalStorage() {
-    return JSON.parse(localStorage.getItem("menu"))
-  },
-}
+import { $ } from "./utils/dom.js";
+import store from "./store/index.js";
 
 function App() {
 
@@ -29,6 +21,7 @@ function App() {
       this.savedMenu = store.getLocalStorage();
       render();
     };
+    initEventListeners();
   };
 
   // 메뉴판 그리기
@@ -106,55 +99,58 @@ function App() {
 
   // 메뉴 카운트 기능
   const menuCount = () => {
-    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    const menuCount = this.savedMenu[this.currentMenu].length;
     $(".menu-count").innerText = `총 ${menuCount}개`;
   };
 
-  // 메뉴판 클릭
-  $("nav").addEventListener("click", (e) => {
-    const isMenuButton = e.target.classList.contains("cafe-category-name");
-    if (isMenuButton) {
-      this.currentMenu = e.target.dataset.categoryName;
-      let h2 = e.target.closest("button").innerText;
-      $("#h2").innerText = `${h2} 메뉴 관리`;
-      render();
-    };
-  });
+  const initEventListeners = () => {
 
-  // 버튼 클릭 메뉴 추가
-  $("#espresso-menu-submit-button").addEventListener("click", () => {
-    addMenu();
-    menuCount();
-  });
-
-  // 엔터키 메뉴 추가
-  $("#espresso-menu-name").addEventListener("keypress", (e) => {
-    if (e.key === 'Enter') {
+    // 버튼 클릭 메뉴 추가
+    $("#espresso-menu-submit-button").addEventListener("click", () => {
       addMenu();
-      menuCount();
-    };
-  });
+    });
 
-  // 품절, 수정, 삭제 이벤트
-  $("#espresso-menu-list").addEventListener("click", (e) => {
-    if (e.target.classList.contains("menu-sold-out-button")) {
-      soldOutMenu(e);
-      return;
-    };
-    if (e.target.classList.contains("menu-edit-button")) {
-      updateMenu(e);
-      return;
-    };
-    if (e.target.classList.contains("menu-remove-button")) {
-      removeMenu(e);
-      return;
-    };
-  });
+    // 엔터키 메뉴 추가
+    $("#espresso-menu-name").addEventListener("keypress", (e) => {
+      if (e.key === 'Enter') {
+        addMenu();
+      };
+    });
 
-  // submit 기본 동작 무효화
-  $("#espresso-menu-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-  });
+    // 메뉴판 클릭
+    $("nav").addEventListener("click", (e) => {
+      const isMenuButton = e.target.classList.contains("cafe-category-name");
+      if (isMenuButton) {
+        this.currentMenu = e.target.dataset.categoryName;
+        let h2 = e.target.closest("button").innerText;
+        $("#h2").innerText = `${h2} 메뉴 관리`;
+        render();
+      };
+    });
+
+    // 품절, 수정, 삭제 이벤트
+    $("#espresso-menu-list").addEventListener("click", (e) => {
+      if (e.target.classList.contains("menu-sold-out-button")) {
+        soldOutMenu(e);
+        return;
+      };
+      if (e.target.classList.contains("menu-edit-button")) {
+        updateMenu(e);
+        return;
+      };
+      if (e.target.classList.contains("menu-remove-button")) {
+        removeMenu(e);
+        return;
+      };
+    });
+
+    // submit 기본 동작 무효화
+    $("#espresso-menu-form").addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
+  };
+
 };
 
 const app = new App();
