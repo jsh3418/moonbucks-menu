4cf720f323620311dacc87648e3a6d175dcf74d0
seonghun <albert5428@gmail.com>
2022-05-15 18:18:14 +0900

    STEP-1 요구사항5

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,14 @@
 // [V]에스프레소 메뉴에 새로운 메뉴를 엔터키 입력으로 추가한다.
 // [V]메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // [V]사용자 입력값이 빈 값이라면 추가되지 않는다.
-// [ ]메뉴 수정 버튼을 눌러 메뉴 이름을 수정할 수 있게 한다. 브라우저에서 제공하는 prompt 인터페이스를 활용한다.
+// [V]메뉴 수정 버튼을 눌러 메뉴 이름을 수정할 수 있게 한다. 브라우저에서 제공하는 prompt 인터페이스를 활용한다.
 // [ ]메뉴 삭제 버튼을 이용하여 메뉴 삭제할 수 있다. 브라우저에서 제공하는 confirm 인터페이스를 활용한다.
 // [ ]총 메뉴 갯수를 count하여 상단에 보여준다.
 
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-
+  // 메뉴명 추가 기능
   const addMenuName = () => {
     const menuName = $("#espresso-menu-name").value;
     if (menuName === "") {
@@ -41,16 +41,27 @@ function App() {
     $("#espresso-menu-list").insertAdjacentHTML('beforeend', template);
     $("#espresso-menu-name").value = "";
   };
+  // 메뉴명 수정 기능
+  const updateMenuName = (e) => {
+    const updatedMenuName = prompt("수정할 메뉴명을 입력해주세요.", e.target.closest("li").querySelector(".menu-name").innerText);
+    e.target.closest("li").querySelector(".menu-name").innerText = updatedMenuName;
+  };
 
-  // 확인 버튼 클릭 이벤트
+  // 확인 버튼 메뉴 추가 이벤트
   $("#espresso-menu-submit-button").addEventListener("click", () => {
     addMenuName();
   });
-
+  // 엔터로 메뉴 추가 이벤트, form 기본 동작 무효
   $("#espresso-menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
     addMenuName();
-  })
+  });
+  // 수정 버튼 이벤트
+  $("#espresso-menu-list").addEventListener("click", (e) => {
+    if (e.target.classList.contains("menu-edit-button")) {
+      updateMenuName(e);
+    };
+  });
 
 };
 
